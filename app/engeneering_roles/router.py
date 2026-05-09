67bbd206c82258ff04ab4bd9456c8a14e587d02c
smiana123@gmail.com <smiana123@gmail.com>
2026-05-09 15:15:47 +0300

    private route

--- app/engeneering_roles/router.py
+++ app/engeneering_roles/router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Response, Depends
 from app.auth.models import User
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.dependencies.auth_dep import get_current_user, get_current_admin_user, check_refresh_token
-from app.dependencies.dao_dep import get_session_with_commit, get_session_without_commit
 
 router = APIRouter()
 
@@ -17,8 +15,7 @@ ENGINEER_ROLES = [
 ]
 
 @router.get("/")
-async def read_category_by_query(title: str, session: AsyncSession = Depends(get_session_with_commit),
-                        user_data: User = Depends(get_current_admin_user)):
+async def read_category_by_query(title: str):
     role_to_return = None
     for role in ENGINEER_ROLES:
         if role.get('title').casefold() == title.casefold():
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from loguru import logger
@@ -8,7 +8,7 @@ from loguru import logger
 from app.auth.router import router as router_auth
 from app.engeneering_roles.router import router as engineering_roles_router
 
-from app.dependencies.auth_dep import get_current_user 
+from app.dependencies.auth_dep import get_current_user
 
 
 @asynccontextmanager
@@ -71,8 +71,12 @@ def register_routers(app: FastAPI) -> None:
     # Подключение роутеров
     app.include_router(root_router, tags=["root"])
     app.include_router(router_auth, prefix='/auth', tags=['Auth'])
-    app.include_router(engineering_roles_router, prefix='/engineering_roles', tags=['Engineering Roles'])
-
+    app.include_router(
+        engineering_roles_router,
+        prefix='/engineering_roles',
+        tags=['Engineering Roles'],
+        dependencies=[Depends(get_current_user)] # Глобальная проверка для этого роутера
+    )
 
 # Создание экземпляра приложения
 app = create_app()
